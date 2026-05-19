828110befed9188d9d49b48498fa7d2e04da35cd
ridelyEX <arturovalruiz@gmail.com>
2026-05-19 14:09:35 -0600

    DependencyInjection y AgeoContext creado y configurado

--- Data/Context/AgeoContext.cs
+++ Data/Context/AgeoContext.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models.Entities.Personas;
 
 namespace Data.Context
 {
-    internal class AgeoContext
+    public class AgeoContext : DbContext
     {
+        public DbSet<Persona> Personas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // modelBuilder temporal de persona, corrección próxima
+            modelBuilder.Entity<Persona>()
+                .HasKey(p => p.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- Data/DependencyInjection.cs
+++ Data/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,31 @@ using System.Threading.Tasks;
 
 namespace Data
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found");
+
+            services.AddDbContext<AgeoContext>(options =>
+                options.UseMySql(
+                    connectionString,
+                    ServerVersion.AutoDetect(connectionString),
+                    mysqlOptions =>
+                    {
+                        mysqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SingleQuery);
+                        mysqlOptions.EnableRetryOnFailure(
+                            maxRetryCount: 3,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null
+                        );
+                    }
+                ),
+                ServiceLifetime.Scoped
+            );
+            return services;
+        }
+
     }
 }
